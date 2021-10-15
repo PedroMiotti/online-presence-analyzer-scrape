2f741eda5bb50f77197cf91e6becce285f785c6d
Thiago Miotti <thiago.miotti10@gmail.com>
2021-10-14 23:17:40 -0300

    Fixing a bug on run_all() function

--- app.py
+++ app.py
@@ -8,20 +8,20 @@ app = Flask(__name__)
 @app.route('/run', methods=["GET"])
 def run_all():
     allCompanies = ["picpay", "correios"]
-    data = {}
+    data = []
 
     factory = CompanyFactory()
-    for name, comp in allCompanies.items():
-        data[name] = factory.scrape(name)
+    for company in allCompanies:
+        data.append(factory.scrape(company))
 
-    return response(200, "Sucesso", "dados", data)
+    return response(200, "Sucesso", "companies", data)
 
 
 @app.route('/run/<company_name>', methods=["GET"])
 def run_one(company_name):
     factory = CompanyFactory()
-    dados = factory.scrape(company_name)
-    return response(200, "Sucesso", "companies", dados)
+    data = factory.scrape(company_name)
+    return response(200, "Sucesso", "companies", data)
 
 
 if __name__ == '__main__':
